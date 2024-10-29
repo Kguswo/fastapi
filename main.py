edad1d724dc4c006821a7f39662966d280ceaaf8
김현재 <nowalex322@naver.com>
2024-10-29 10:59:08 +0900

    feat: Update main.py with Request Body

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from math import trunc
 from pyexpat.errors import messages
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -81,3 +83,24 @@ async def get_user_item(user_id: int, item_id: str, q: str | None = None, short:
             }
         )
     return item
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: int
+    tax: float | None = None
+
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price * item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
