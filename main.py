44099ca6634b39e50496a5b7e74110fc87351c34
김현재 <nowalex322@naver.com>
2024-10-29 13:38:17 +0900

    feat: Update main.py with path parameter and numeric validation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from enum import Enum
 from math import trunc
 from pyexpat.errors import messages
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from pydantic import BaseModel
 from typing import Optional
 
@@ -129,3 +129,16 @@ async def hidden_query_route(hidden_query: str | None = Query(None, include_in_s
     if hidden_query:
         return {"hidden_query": hidden_query}
     return {"hidden_query": "Not Found"}
+
+@app.get("/items_validation/{item_id}")
+async def read_items_validation(
+    *,
+    item_id: int = Path(..., title="The ID of a item to get", ge=10, le=100),
+    q: str = "hello",
+        size: float = Query(..., gt=0, lt=9.87)
+):
+
+    resutls = {"item_id": item_id, "size": size}
+    if q:
+        resutls.update({"q": q})
+    return resutls
